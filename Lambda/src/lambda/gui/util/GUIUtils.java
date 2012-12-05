53b7a3d8cb0b571fd8ef106f2db1d8f2ef4c3bc1
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2012-12-05 14:14:40 +0900

    add layout setter

--- Lambda/src/lambda/gui/util/GUIUtils.java
+++ Lambda/src/lambda/gui/util/GUIUtils.java
@@ -1,11 +1,35 @@
 package lambda.gui.util;
 
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.GraphicsEnvironment;
 
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.GroupLayout.ParallelGroup;
+import javax.swing.GroupLayout.SequentialGroup;
 import javax.swing.UIManager;
 
 public final class GUIUtils
 {
+	public static void setVerticalLayout(Container host, Component ... components)
+	{
+		GroupLayout gl = new GroupLayout(host);
+		host.setLayout(gl);
+		gl.setAutoCreateContainerGaps(true);
+		gl.setAutoCreateGaps(true);
+
+		SequentialGroup sg = gl.createSequentialGroup();
+		ParallelGroup pg = gl.createParallelGroup(Alignment.LEADING);
+		for (Component c : components)
+		{
+			sg = sg.addComponent(c);
+			pg = pg.addComponent(c, 0, GroupLayout.PREFERRED_SIZE, Short.MAX_VALUE);
+		}
+		gl.setVerticalGroup(sg);
+		gl.setHorizontalGroup(pg);
+	}
+
 	public static void setLookAndFeelToSystem()
 	{
 		try
