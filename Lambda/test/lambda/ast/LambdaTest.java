2e4d6302be1ffe2d83f03d9c45c52d0f275d3321
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2014-05-18 12:38:52 +0900

    Add test cases

--- Lambda/test/lambda/ast/LambdaTest.java
+++ Lambda/test/lambda/ast/LambdaTest.java
@@ -1,7 +1,10 @@
 package lambda.ast;
 
+import lambda.ast.parser.ParserException;
+
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class LambdaTest
@@ -14,5 +17,20 @@ public class LambdaTest
 		assertTrue(lambda1.equals(lambda2));
 		assertTrue(lambda2.equals(lambda1));
 		assertTrue(lambda1.hashCode() == lambda2.hashCode());
+	}
+
+	@Test(expected=ParserException.class)
+	public void parseEmpty() throws ParserException
+	{
+		Lambda.parse("");
+	}
+
+	@Test
+	public void parseTest() throws ParserException
+	{
+		Lambda lambda = Lambda.literal("x").app(Lambda.literal("y").app(Lambda.macro("m"))).abs("x").abs("y");
+		assertEquals(lambda, Lambda.parse("\\y.\\x.x(y<m>)"));
+		assertEquals(lambda, Lambda.parse("\\yx.x(y<m>)"));
+		assertEquals(lambda, Lambda.parse("  \\ y x . x ( y <m> )   "));
 	}
 }
