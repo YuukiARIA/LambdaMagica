94c6a3291d65faf4dca54338592018bbd795d8bd
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2015-06-28 03:35:15 +0900

    Add methods to detect recursive definition

--- Lambda/src/lambda/macro/MacroDefinition.java
+++ Lambda/src/lambda/macro/MacroDefinition.java
@@ -1,10 +1,14 @@
 package lambda.macro;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import lambda.ast.Lambda;
+import lambda.ast.MacroNameCollector;
+import util.LoopDetector;
 
 public class MacroDefinition
 {
@@ -43,5 +47,22 @@ public class MacroDefinition
 	public Map<String, Lambda> getDefinedMacros()
 	{
 		return Collections.unmodifiableMap(macros);
+	}
+
+	public Map<String, Set<String>> getMacroDependencies()
+	{
+		Map<String, Set<String>> dependencies = new HashMap<String, Set<String>>();
+		for (Map.Entry<String, Lambda> entry : macros.entrySet())
+		{
+			Set<String> usedNames = MacroNameCollector.collectMacroName(entry.getValue());
+			dependencies.put(entry.getKey(), usedNames);
+		}
+		return dependencies;
+	}
+
+	public Set<String> detectRecursiveDefinitions()
+	{
+		LoopDetector<String> detector = LoopDetector.create(getMacroDependencies());
+		return detector.detectCyclicLoop();
 	}
 }
