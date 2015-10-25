36429326a71f3f461ee9728228e270cb343c6458
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2015-10-25 22:41:57 +0900

    Revise build-mac-app script to use assets

--- Lambda/build-mac-app.sh
+++ Lambda/build-mac-app.sh
@@ -12,48 +12,8 @@ ant jar
 
 mkdir -p ${CONTENTS}/MacOS ${CONTENTS}/Resources
 
-cat <<'EOF' > ${EXECUTABLE}
-#!/bin/sh
-CD=$(cd `dirname $0`; pwd)
-APP_ROOT=$(cd ${CD}/../../..; pwd)
-java \
-  -Xdock:name="LambdaMagica"         \
-  -Dapple.laf.useScreenMenuBar=true  \
-  -Duser.dir="${APP_ROOT}"           \
-  -jar ${CD}/lm.jar &
-EOF
-
+cp assets/osx/run.sh ${EXECUTABLE}
 chmod +x ${EXECUTABLE}
 
 cp lambda-magica/lm.jar ${CONTENTS}/MacOS
-
-cat <<EOF > ${CONTENTS}/Info.plist
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-  <dict>
-    <key>CFBundleExecutable</key>
-    <string>run.sh</string>
-    <key>CFBundleIconFile</key>
-    <string></string>
-    <key>CFBundleIdentifier</key>
-    <string></string>
-    <key>CFBundleDisplayName</key>
-    <string>LambdaMagica</string>
-    <key>CFBundleInfoDictionaryVersion</key>
-    <string>6.0</string>
-    <key>CFBundleName</key>
-    <string>LambdaMagica</string>
-    <key>CFBundlePackageType</key>
-    <string>APPL</string>
-    <key>CFBundleShortVersionString</key>
-    <string></string>
-    <key>CFBundleSignature</key>
-    <string>????</string>
-    <key>CFBundleVersion</key>
-    <string>1</string>
-    <key>NSHumanReadableCopyright</key>
-    <string>Copyright (C) YuukiARIA</string>
-  </dict>
-</plist>
-EOF
+cp assets/osx/Info.plist ${CONTENTS}
